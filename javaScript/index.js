a82445fa6d0c138991bb87f8a7f4994a22d3e76d
macupon <macupon@gmail.com>
2022-12-05 10:47:44 +0100

    first commit

---
+++ javaScript/index.js
@@ -0,0 +1,24 @@
+let inoculacion;
+let hidratacion;
+let sal;
+
+document.getElementsByClassName("")
+
+let val_farina = document.querySelector('input[name="sourInput"]:checked');
+let val_farinaa = document.getElementById("sourInput")
+let text_sour = document.getElementById("sour_label")
+
+alert("yes")
+
+text_sour.addEventListener("click", () => {
+
+    // num = document.getElementById("barraIno").value
+
+
+    // document.getElementsByClassName("progress-label").style()= "counter-reset: num 25"
+    alert(val_farina)
+
+})
+
+
+
